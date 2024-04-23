210bce699874fea6b777c984eafac4334acd0969
Иорданов Павел Алексеевич <paiordanov@edu.hse.ru>
2024-04-23 22:06:29 +0300

    Структура приложения

---
+++ GradePredictor/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  GradePredictor
+//
+//  Created by Иорданов Павел on 23.04.2024.
+//
+
+import Foundation
---
+++ GradePredictor/SubjectsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SubjectsViewController.swift
+//  GradePredictor
+//
+//  Created by Иорданов Павел on 23.04.2024.
+//
+
+import UIKit
+
+class SubjectsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
